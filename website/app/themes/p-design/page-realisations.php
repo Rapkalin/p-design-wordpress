c5e46001adf197405b2ec6125c034e18a0962eb9
Raphael Kalinowski <raphael@myminicom.com>
2023-09-26 00:35:58 +0200

    init dynamisation for realisations page

--- website/app/themes/p-design/page-realisations.php
+++ website/app/themes/p-design/page-realisations.php
@@ -32,10 +32,10 @@ get_header();
 					<div class="content-with-menus">
 						<div class="menus">
 							<a href="#" class="active">Tous les projets</a>
-							<a href="#">Bar</a>
-							<a href="#">Brasserie</a>
-							<a href="#">Restaurant</a>
-							<a href="#">Hôtel</a>
+							<a href="#category-bar">Bar</a>
+							<a href="#category-brasserie">Brasserie</a>
+							<a href="#category-restaurant">Restaurant</a>
+							<a href="#category-hotel">Hôtel</a>
 						</div>
 						<div class="row">
 							<div class="left">
@@ -49,76 +49,188 @@ get_header();
 				</div>
 			</div>
 
+
 			<section class="realisations-list">
 				<div class="large-container">
 					<div class="realisations-categories">
 
-						<div class="category">
+                        <?php
+                            $categories = get_terms('categories-realisations');
+
+                            foreach ($categories as $category) :
+                                $category_slug = $category->slug; // Remplacez 'votre-terme' par le slug du terme que vous souhaitez récupérer
+                                $category = get_term_by('slug', $category_slug, 'categories-realisations'); // Remplacez 'category' par le nom de la taxonomie appropriée (category ou post_tag)
+
+                                if ($category) {
+                                    $args = array(
+                                        'posts_per_page' => -1, // Récupérer tous les articles liés au terme
+                                        'tax_query' => array(
+                                            array(
+                                                'taxonomy' => $category->taxonomy,
+                                                'field'    => 'id',
+                                                'terms'    => $category->term_id,
+                                            ),
+                                        ),
+                                    );
+                        ?>
+
+
+                                        <div class="category">
+                                            <div class="category-title">
+                                                <h2 id="category-<?= $category->name ?>"><?= $category->count . ' ' . $category->name ?></h2>
+                                                <p>Ecus vellique quis in consequi blandit et aceri dolupta nobit facerunt as maximagnis dolorem porest facillum abores xero officitae volupitatem hit officia dolorum et velique.</p>
+                                            </div>
+                                            <div class="category-realisations">
+                                                <?php
+                                                    $query = new WP_Query($args);
+
+                                                    if ($query->have_posts()) {
+                                                        $i = 1;
+                                                        while ($query->have_posts()) {
+                                                        $query->the_post();
+                                                        $image = get_field('realisation_banner', get_the_ID());
+                                                ?>
+                                                        <div class="realisation">
+                                                            <div class="number-ratio">
+                                                                <span class="big"><?= $i; ?></span>
+                                                                <span class="big"><?= $category->count ?> </span>
+                                                            </div>
+                                                                <a href="<?= the_permalink(); ?>">
+                                                                    <div class="image-container">
+                                                                        <div class="image" style="background-image: url(<?= $image['url'] ?>);">
+                                                                            <div class="hover"><img src="<?= asset('plus.svg'); ?>" alt=""></div>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="name"><?= the_title() . ' | Paris'; ?> </div>
+                                                                </a>
+                                                        </div>
+                                                <?php
+                                                            $i++;
+                                                        }
+                                                        wp_reset_postdata(); // Réinitialisez les données de l'article après la boucle
+                                                    } else {
+                                                        echo 'Aucun article trouvé pour ce terme.';
+                                                    }
+                                                ?>
+                                                <div class="realisation">
+                                                    <div class="more">
+                                                        <a href="#" class="button button-white button-image">
+                                                            <img src="<?= asset('plus.svg') ?>" class="svg">
+                                                        </a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                            <?php
+
+                                } else {
+                                        echo 'Terme introuvable.';
+                                }
+                            endforeach;
+                        ?>
+
+
+<!--						<div class="category">
 							<div class="category-title">
-								<h2>15 Bars</h2>
+								<h2 id="category-brasserie">6 Brasseries</h2>
 								<p>Ecus vellique quis in consequi blandit et aceri dolupta nobit facerunt as maximagnis dolorem porest facillum abores xero officitae volupitatem hit officia dolorum et velique.</p>
 							</div>
 							<div class="category-realisations">
-								<?php for ($i=1; $i <= 7; $i++) : ?>
+								<?php /*for ($i=1; $i <= 7; $i++) : */?>
 									<div class="realisation">
 										<div class="number-ratio">
-											<span class="big"><?= $i; ?></span>
+											<span class="big"><?php /*= $i; */?></span>
 											<span class="big">15</span>
 										</div>
-										<a href="<?= home_url('realisations/le-choupinet/'); ?>">
+										<a href="<?php /*= home_url('realisations/le-choupinet/'); */?>">
 											<div class="image-container">
-												<div class="image" style="background-image: url(<?= asset('home-showroom.jpg') ?>);">
-													<div class="hover"><img src="<?= asset('plus.svg'); ?>" alt=""></div>
+												<div class="image" style="background-image: url(<?php /*= asset('home-showroom.jpg') */?>);">
+													<div class="hover"><img src="<?php /*= asset('plus.svg'); */?>" alt=""></div>
 												</div>
 											</div>
 											<div class="name">Ismael | Paris</div>
 										</a>
 									</div>
-								<?php endfor; ?>
+								<?php /*endfor; */?>
 								<div class="realisation">
 									<div class="more">
 										<a href="#" class="button button-white button-image">
-											<img src="<?= asset('plus.svg') ?>" class="svg">
+											<img src="<?php /*= asset('plus.svg') */?>" class="svg">
 										</a>
 									</div>
 								</div>
 							</div>
 						</div>
 
-						<div class="category">
-							<div class="category-title">
-								<h2>6 Brasseries</h2>
-								<p>Ecus vellique quis in consequi blandit et aceri dolupta nobit facerunt as maximagnis dolorem porest facillum abores xero officitae volupitatem hit officia dolorum et velique.</p>
-							</div>
-							<div class="category-realisations">
-								<?php for ($i=1; $i <= 7; $i++) : ?>
-									<div class="realisation">
-										<div class="number-ratio">
-											<span class="big"><?= $i; ?></span>
-											<span class="big">15</span>
-										</div>
-										<a href="<?= home_url('realisations/le-choupinet/'); ?>">
-											<div class="image-container">
-												<div class="image" style="background-image: url(<?= asset('home-showroom.jpg') ?>);">
-													<div class="hover"><img src="<?= asset('plus.svg'); ?>" alt=""></div>
-												</div>
-											</div>
-											<div class="name">Ismael | Paris</div>
-										</a>
-									</div>
-								<?php endfor; ?>
-								<div class="realisation">
-									<div class="more">
-										<a href="#" class="button button-white button-image">
-											<img src="<?= asset('plus.svg') ?>" class="svg">
-										</a>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+                        <div class="category">
+                            <div class="category-title">
+                                <h2 id="category-restaurant">6 Restaurants</h2>
+                                <p>Ecus vellique quis in consequi blandit et aceri dolupta nobit facerunt as maximagnis dolorem porest facillum abores xero officitae volupitatem hit officia dolorum et velique.</p>
+                            </div>
+                            <div class="category-realisations">
+                                <?php /*for ($i=1; $i <= 7; $i++) : */?>
+                                    <div class="realisation">
+                                        <div class="number-ratio">
+                                            <span class="big"><?php /*= $i; */?></span>
+                                            <span class="big">15</span>
+                                        </div>
+                                        <a href="<?php /*= home_url('realisations/le-choupinet/'); */?>">
+                                            <div class="image-container">
+                                                <div class="image" style="background-image: url(<?php /*= asset('home-showroom.jpg') */?>);">
+                                                    <div class="hover"><img src="<?php /*= asset('plus.svg'); */?>" alt=""></div>
+                                                </div>
+                                            </div>
+                                            <div class="name">Ismael | Paris</div>
+                                        </a>
+                                    </div>
+                                <?php /*endfor; */?>
+                                <div class="realisation">
+                                    <div class="more">
+                                        <a href="#" class="button button-white button-image">
+                                            <img src="<?php /*= asset('plus.svg') */?>" class="svg">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="category">
+                            <div class="category-title">
+                                <h2 id="category-hotel">6 Hotels</h2>
+                                <p>Ecus vellique quis in consequi blandit et aceri dolupta nobit facerunt as maximagnis dolorem porest facillum abores xero officitae volupitatem hit officia dolorum et velique.</p>
+                            </div>
+                            <div class="category-realisations">
+                                <?php /*for ($i=1; $i <= 7; $i++) : */?>
+                                    <div class="realisation">
+                                        <div class="number-ratio">
+                                            <span class="big"><?php /*= $i; */?></span>
+                                            <span class="big">15</span>
+                                        </div>
+                                        <a href="<?php /*= home_url('realisations/le-choupinet/'); */?>">
+                                            <div class="image-container">
+                                                <div class="image" style="background-image: url(<?php /*= asset('home-showroom.jpg') */?>);">
+                                                    <div class="hover"><img src="<?php /*= asset('plus.svg'); */?>" alt=""></div>
+                                                </div>
+                                            </div>
+                                            <div class="name">Ismael | Paris</div>
+                                        </a>
+                                    </div>
+                                <?php /*endfor; */?>
+                                <div class="realisation">
+                                    <div class="more">
+                                        <a href="#" class="button button-white button-image">
+                                            <img src="<?php /*= asset('plus.svg') */?>" class="svg">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>-->
+
+                    </div>
 				</div>
 			</section>
+
 
 			<section class="realisations-conclusion">
 				<div class="large-container">
