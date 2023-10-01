f607957ae883d9c20faf600de6e89a7f6ac50250
Rapkalin <raphael@myminicom.com>
2023-10-01 21:31:06 +0200

    refonte-404 - Design done

--- website/app/themes/p-design/404.php
+++ website/app/themes/p-design/404.php
@@ -1,7 +1,11 @@
 <?php
 get_header();
 ?>
-
+    <div class="pdesign-404">
+        <h2 class="pdesign-404-title">Oups, aucun contenu n'a été trouvé</h2>
+        <p class="pdesign-404-text">Il semblerait que le contenu recherché n'existe plus.</p>
+        <a href="" class="pdesign-404-button">Retour à l'accueil</a>
+    </div>
 
 <?php
 get_footer();
