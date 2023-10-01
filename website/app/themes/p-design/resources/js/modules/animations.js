7e3a55840edf6f0324394838de6005fdbca3b7ae
Rapkalin <raphael@myminicom.com>
2023-10-01 20:18:04 +0200

    refonte-team - Update js, css and add key numbers bloc

--- website/app/themes/p-design/resources/js/modules/animations.js
+++ website/app/themes/p-design/resources/js/modules/animations.js
@@ -1,11 +1,11 @@
 $(function() {
-	if ($('.home').length > 0) {
+	if ($('.page-l-equipe').length > 0) {
 		var animationAboutScrolled = false;
 
 		$(document).scroll(function() {
 			if (!animationAboutScrolled) {
 				var $scroll = $(window).scrollTop(),
-					$homeAbout = $('#home-about').offset().top,
+					$homeAbout = $('#team-about').offset().top,
 					$windowHeight = $(window).height();
 
 				if ($scroll > ($homeAbout - $windowHeight / 2)) {
