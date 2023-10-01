1a2a98bb2b6efac2941fd4fea9ab6f58a5fa22d1
Rapkalin <raphael@myminicom.com>
2023-10-01 21:40:07 +0200

    refonte-404 - Forgot button link

--- website/app/themes/p-design/404.php
+++ website/app/themes/p-design/404.php
@@ -4,7 +4,7 @@ get_header();
     <div class="pdesign-404">
         <h2 class="pdesign-404-title">Oups, aucun contenu n'a été trouvé</h2>
         <p class="pdesign-404-text">Il semblerait que le contenu recherché n'existe plus.</p>
-        <a href="" class="pdesign-404-button">Retour à l'accueil</a>
+        <a href="/" class="pdesign-404-button">Retour à l'accueil</a>
     </div>
 
 <?php
